191d216505699185e8f8ceccaee3daa11d972b02
gastatouille <bouchergaston4@gmail.com>
2022-07-06 21:22:49 +1000

    created intake subsystem

---
+++ src/main/include/subsystems/SubIntake.h
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc/DoubleSolenoid.h>
+#include <rev/CANSparkMax.h>
+
+class SubIntake : public frc2::SubsystemBase {
+ public:
+  SubIntake();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+ private:
+  rev::CANSparkMax _spmIntakeSpin{9, rev::CANSparkMaxLowLevel::MotorType::kBrushless}; 
+  frc::DoubleSolenoid _solntakeDeploy {frc::PneumaticsModuleType::CTREPCM, 1, 2};
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+};
