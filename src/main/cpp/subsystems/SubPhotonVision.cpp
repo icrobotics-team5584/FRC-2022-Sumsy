170648bab2169773ebc46489602cf3b05c068cd5
gastatouille <bouchergaston4@gmail.com>
2022-08-31 20:23:24 +1000

    PhotonVision Added Camera communication to dashboard

--- src/main/cpp/subsystems/SubPhotonVision.cpp
+++ src/main/cpp/subsystems/SubPhotonVision.cpp
@@ -3,8 +3,32 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/SubPhotonVision.h"
+#include <photonlib/PhotonUtils.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 SubPhotonVision::SubPhotonVision() = default;
 
 // This method will be called once per scheduler run
-void SubPhotonVision::Periodic() {}
+void SubPhotonVision::Periodic() {
+
+    photonlib::PhotonPipelineResult result = camera.GetLatestResult();
+    
+
+    if (result.HasTargets()) {
+      //First calculate range
+      HasTarget = true;
+      frc::SmartDashboard::PutBoolean("PhotonVisionTargetFound", HasTarget);
+      units::meter_t range = photonlib::PhotonUtils::CalculateDistanceToTarget(CAMERA_HEIGHT, TARGET_HEIGHT, CAMERA_PITCH,units::degree_t{result.GetBestTarget().GetPitch()});
+       frc::SmartDashboard::PutNumber("PhotonVisionRangeToTarget", double(range));
+
+      // Use this range as the measurement we give to the PID controller.
+      //forwardSpeed =-controller.Calculate(range.value(), GOAL_RANGE_METERS.value());
+          
+    } else {
+      // If we have no targets, stay still.
+      //forwardSpeed = 0;
+      HasTarget = false;
+       frc::SmartDashboard::PutBoolean("PhotonVisionTargetFound", HasTarget);
+    }
+
+}
