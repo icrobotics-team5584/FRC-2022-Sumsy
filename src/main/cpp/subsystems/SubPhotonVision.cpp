6dc3a69b81d6d7352d5548ced753488dbdf520e4
Liam Stow <liam.stow@outlook.com>
2022-10-24 19:07:29 +1100

    Updated photonlib to 2023.1.1-beta-3

--- src/main/cpp/subsystems/SubPhotonVision.cpp
+++ src/main/cpp/subsystems/SubPhotonVision.cpp
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #include "subsystems/SubPhotonVision.h"
 #include <photonlib/PhotonUtils.h>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -10,32 +6,17 @@ SubPhotonVision::SubPhotonVision() = default;
 
 // This method will be called once per scheduler run
 void SubPhotonVision::Periodic() {
-
     photonlib::PhotonPipelineResult result = camera.GetLatestResult();
     
-
     if (result.HasTargets()) {
-      //First calculate range
       auto bestTarget = result.GetBestTarget();
-      HasTarget = true;
-      frc::SmartDashboard::PutBoolean("PhotonVisionTargetFound", HasTarget);
-     /* units::meter_t range = photonlib::PhotonUtils::CalculateDistanceToTarget(CAMERA_HEIGHT, TARGET_HEIGHT, CAMERA_PITCH,units::degree_t{result.GetBestTarget().GetPitch()});
-      frc::SmartDashboard::PutNumber("PhotonVisionRangeToTarget", double(range));
-      frc::SmartDashboard::PutNumber("LimeLightYaw", result.GetBestTarget().GetYaw()); */
-      frc::SmartDashboard::PutNumber("target Distance", bestTarget.GetCameraRelativePose().X().value()); 
-      frc::SmartDashboard::PutNumber("target Rotation", bestTarget.GetCameraRelativePose().Rotation().Degrees().value()); 
-      frc::SmartDashboard::PutNumber("target Y", bestTarget.GetCameraRelativePose().Y().value()); 
+      auto botToTarg = bestTarget.GetCameraToTarget();
 
-      // Use this range as the measurement we give to the PID controller.
-      //forwardSpeed =-controller.Calculate(range.value(), GOAL_RANGE_METERS.value());
-          
-    } else {
-      // If we have no targets, stay still.
-      //forwardSpeed = 0;
-      HasTarget = false;
-       frc::SmartDashboard::PutBoolean("PhotonVisionTargetFound", HasTarget);
+      frc::SmartDashboard::PutNumber("target X", botToTarg.X().value());
+      frc::SmartDashboard::PutNumber("target Rot",
+                                     botToTarg.Rotation().Z().value());
+      frc::SmartDashboard::PutNumber("target Y", botToTarg.Y().value());
     }
-
 }
 
 units::meter_t SubPhotonVision::GetX() {
@@ -43,7 +24,7 @@ units::meter_t SubPhotonVision::GetX() {
   
     if (result.HasTargets()) {
       auto bestTarget = result.GetBestTarget();
-      return bestTarget.GetCameraRelativePose().X();
+      return bestTarget.GetCameraToTarget().X();
     } else {
       return 0_m;
     }
--- src/main/include/subsystems/SubPhotonVision.h
+++ src/main/include/subsystems/SubPhotonVision.h
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
@@ -16,28 +12,19 @@ public:
     static SubPhotonVision inst;
     return inst;
   }
-  SubPhotonVision();
 
   units::meter_t GetX();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
   void Periodic() override;
 
 private:
+  SubPhotonVision();
+  
+  // Camera data
+  photonlib::PhotonCamera camera{"limelight"};
   const units::meter_t CAMERA_HEIGHT = 0.8_m;
-  const units::meter_t TARGET_HEIGHT = 1_m;
-  bool HasTarget = false;
-
-  // Angle between horizontal and the camera.
   const units::radian_t CAMERA_PITCH = 15_deg;
 
-  // How far from the target we want to be
-  const units::meter_t GOAL_RANGE_METERS = 0.2_m;
-
-  photonlib::PhotonCamera camera{"limelight"};
-
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+  // Target data
+  const units::meter_t TARGET_HEIGHT = 1_m;
 };
